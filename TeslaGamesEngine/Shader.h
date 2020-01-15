438d129414658caca1f7ad5d0de983b256ac4161
JDhilon <jashanjotdhilon@gmail.com>
2020-01-15 13:12:56 -0700

    Added basic rendering functionality (shaders, window, and indexed mesh). Running the program now shows 2 multi-coloured triangles on screen

--- TeslaGamesEngine/Shader.h
+++ TeslaGamesEngine/Shader.h
@@ -1,25 +1,45 @@
 #pragma once
 
+#include <stdio.h>
 #include <string>
+#include <iostream>
+#include <fstream>
+
+#include <GL\glew.h>
 
 /*
- * Class handling loading of and applying shaders while rendering
+ * Class responsible for loading shaders and aplpying them during rendering
  */
 class Shader
 {
 public:
 	Shader();
-	// Loads a shader from a string of the code (useful for having a "base" shader
-	void LoadFromString(std::string shaderCode);
-	// Loads a shader from a specified file
-	void LoadFromFile(std::string filename);
-	// Uses the shader to render 
+
+	// Create a shader from hard coded functions
+	void CreateFromString(const char* vertexCode, const char* fragmentCode);
+	// Load a shader from provided files
+	void CreateFromFiles(const char* vertexLocation, const char* fragmentLocation);
+
+	// Helper method for file IO
+	std::string ReadFile(const char* fileLocation);
+
+	// Accessors for applying uniform locations to the shader
+	GLuint GetProjectionLocation();
+	GLuint GetModelLocation();
+
+	// Use the shader in rendering
 	void UseShader();
-	// Clears the shader and frees memory
+	// Clear the shader from memory + free the memory
 	void ClearShader();
+
 	~Shader();
 
 private:
-	int shaderID;
+	GLuint shaderID, uniformProjection, uniformModel;
+
+	// Helper method for converting shader string to executable code
+	void CompileShader(const char* vertexCode, const char* fragmentCode);
+	// Helper method to add a shader to the shader program
+	void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType);
 };
 
--- TeslaGamesEngine/TeslaGamesEngine.cpp
+++ TeslaGamesEngine/TeslaGamesEngine.cpp
@@ -1,40 +1,112 @@
 // TeslaGamesEngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include "Game.h"
-
-#include <string>
+// The TeslaGamesEngine will be changed to a .lib, not .exe and then be included in the TeslaGames Project
 
 #include "pch.h"
-#include <iostream>
 
-class TestComponent : public Component {
-public:
-	TestComponent() {}
-	~TestComponent() {}
-	void Tick(float deltaTime) {}
-	void Tick(float deltaTime, std::string debugString) {
-		std::cout << "I got this string: ";
-		std::cout << debugString << std::endl;
-	}
-};
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
 
-class TestObject : public Object {
-public:
-	TestComponent component;
-	TestObject() {}
-	~TestObject() {}
-	void Tick(float deltaTime) {
-		std::string str = "Test";
-		component.Tick(deltaTime, str);
-	}
-};
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader.h"
+
+const float toRadians = 3.14159265f / 180.0f;
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+// Vertex Shader
+static const char* vShader = "Shaders/shader.vert";
+
+// Fragment Shader
+static const char* fShader = "Shaders/shader.frag";
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj2);
+}
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
 
 int main()
 {
-	Game mainGame;
-	Object* obj = new TestObject();
-	mainGame.AddObject(obj);
-	mainGame.Play();
-    std::cout << "Hello World!\n";
-	return 1;
+
+	mainWindow = Window(800, 600);
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	GLuint uniformProjection = 0, uniformModel = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
+	// Loop until window closed
+	while (!mainWindow.getShouldClose())
+	{
+		// Get + Handle User Input
+		glfwPollEvents();
+
+		// Clear the window
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+
+		glm::mat4 model = glm::mat4(1.0f);
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 1.0f, -2.5f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		meshList[1]->RenderMesh();
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
 }
