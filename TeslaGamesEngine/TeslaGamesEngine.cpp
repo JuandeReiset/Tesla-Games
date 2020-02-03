f987c1c10f3a30b2fab9e5fd61223fa6e01fa19b
YZ-YI <yiyuzhe0125@outlook.com>
2020-02-02 21:54:26 -0700

    put game footage back

--- TeslaGamesEngine/TeslaGamesEngine.cpp
+++ TeslaGamesEngine/TeslaGamesEngine.cpp
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <cmath>
-#include <iostream>
 #include <vector>
 #include <PhysX/PxPhysicsAPI.h>
 
@@ -17,15 +16,19 @@
 #include <glm\gtc\matrix_transform.hpp>
 #include <glm\gtc\type_ptr.hpp>
 
-#include "Object.h"
-#include "Game.h"
-#include "Vihecle.h"
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader.h"
+#include "Combat.h"
+#include "HealthComponent.h"
 #include "DamagingObject.h"
+#include "Vihecle.h"
+#include "Game.h"
 
 // Stuff for imgui
-#include "ImGui/imgui.h"
-#include "ImGui/imgui_impl_glfw.h"
-#include "ImGui/imgui_impl_opengl3.h"
+#include "imGui/imgui.h"
+#include "imGui/imgui_impl_glfw.h"
+#include "imGui/imgui_impl_opengl3.h"
 
 // end of stuff for imgui
 
@@ -34,7 +37,57 @@
 using namespace physx;
 // End of stuff TA Ben added
 
-int main(void) {
+const float toRadians = 3.14159265f / 180.0f;
+
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+// Vertex Shader
+static const char* vShader = "Shaders/shader.vert";
+
+// Fragment Shader
+static const char* fShader = "Shaders/shader.frag";
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	Mesh* obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj1);
+
+	Mesh* obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 12, 12);
+	meshList.push_back(obj2);
+}
+
+void CreateShaders()
+{
+	Shader* shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+int main()
+{
+
+	const char* glsl_version = "#version 130"; // USED FOR IMGUI SETTING
+	mainWindow = Window(800, 600);
+	mainWindow.Initialise();
+
 	Game mainGame;
 	Object* car = new Vihecle(1);
 	Object* bullet = new DamagingObject(20, 1);
@@ -43,6 +96,131 @@ int main(void) {
 	mainGame.AddObject(bullet);
 
 	mainGame.Play();
+
+	CreateObjects();
+	CreateShaders();
+
+	// Stuff TA Ben added
+	PxDefaultAllocator allocator;
+	PxDefaultErrorCallback errorCallback;
+	// End of stuff TA Ben added
+
+	GLuint uniformProjection = 0, uniformModel = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
+
+	// TODO: Put FPS code into Game.Play()
+	// Loop until window closed
+	double last = glfwGetTime();
+	double now = glfwGetTime();
+
+	glfwSwapInterval(1);
+	// imGui setting BEGINNING
+	/*
+#if __APPLE__
+// GL 3.2 + GLSL 150
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+	// GL 3.0 + GLSL 130
+
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+*/
+// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+
+	ImGui_ImplGlfw_InitForOpenGL(mainWindow.getWindow(), true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// Imgui setting END
+
+	
+	while (!mainWindow.getShouldClose())
+	{
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		/*
+		now = glfwGetTime();
+		double deltaTime = now - last;
+		double fps = 1.0 / deltaTime;
+		last = now;
+		std::cout << "FPS: " << (int) fps << std::endl;
+		*/
+		// Get + Handle User Input
+		glfwPollEvents();
+
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("FPS COUNTER");                          // Create a window called "Hello, world!" and append into it.
+
+			ImGui::Text("Frame per Second counter");               // Display some text (you can use a format strings too)
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+		}
+
+		// Rendering imgui
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(mainWindow.getWindow(), &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		//glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		//glClear(GL_COLOR_BUFFER_BIT);
+
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+
+		glm::mat4 model = glm::mat4(1.0f);
+
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		meshList[0]->RenderMesh();
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 1.0f, -2.5f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		meshList[1]->RenderMesh();
+
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// imgui ends here
+
+		// TODO: Load shader in a material struct in the model (Basically all of the following code refactored to being in model
+		mainWindow.swapBuffers();
+	}
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 
 	return 0;
 }
