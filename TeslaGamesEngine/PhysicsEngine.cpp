fc17c616dc65777b5b2e1b557aea426d807e22c0
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-02-09 15:06:14 -0700

    Vehicle snippet technically there. No more awful link errors.

--- TeslaGamesEngine/PhysicsEngine.cpp
+++ TeslaGamesEngine/PhysicsEngine.cpp
@@ -1,17 +1,487 @@
 #include "PhysicsEngine.h"
-#include "PhysX/PxPhysicsAPI.h"
-
+#include <sstream>
 
 using namespace physx;
+using namespace snippetvehicle;
+
+PhysicsEngine::PhysicsEngine()
+{
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true);
+
+	PxCudaContextManagerDesc cudaContextManagerDesc;
+
+#ifdef RENDER_SNIPPET
+	cudaContextManagerDesc.interopMode = PxCudaInteropMode::OGL_INTEROP;	//Choose interop mode. As the snippets use OGL, we select OGL_INTEROP
+																			//when using D3D, cudaContextManagerDesc.graphicsDevice must be set as the graphics device pointer.
+#else
+	cudaContextManagerDesc.interopMode = PxCudaInteropMode::NO_INTEROP;
+#endif
+
+	gCudaContextManager = PxCreateCudaContextManager(*gFoundation, cudaContextManagerDesc, PxGetProfilerCallback());	//Create the CUDA context manager, required for GRB to dispatch CUDA kernels.
+	if (gCudaContextManager)
+	{
+		if (!gCudaContextManager->contextIsValid())
+		{
+			gCudaContextManager->release();
+			gCudaContextManager = NULL;
+		}
+	}
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(4);			//Create a CPU dispatcher using 4 worther threads
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+
+	sceneDesc.cudaContextManager = gCudaContextManager;		//Set the CUDA context manager, used by GRB.
+
+	sceneDesc.flags |= PxSceneFlag::eENABLE_GPU_DYNAMICS;	//Enable GPU dynamics - without this enabled, simulation (contact gen and solver) will run on the CPU.
+	sceneDesc.flags |= PxSceneFlag::eENABLE_PCM;			//Enable PCM. PCM NP is supported on GPU. Legacy contact gen will fall back to CPU
+	sceneDesc.flags |= PxSceneFlag::eENABLE_STABILIZATION;	//Improve solver stability by enabling post-stabilization.
+	sceneDesc.broadPhaseType = PxBroadPhaseType::eGPU;		//Enable GPU broad phase. Without this set, broad phase will run on the CPU.
+	sceneDesc.gpuMaxNumPartitions = 8;						//Defines the maximum number of partitions used by the solver. Only power-of-2 values are valid. 
+															//A value of 8 generally gives best balance between performance and stability.
+	gScene = gPhysics->createScene(sceneDesc);
+
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	//PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+	//gScene->addActor(*groundPlane);
+
+	ball = CreateDynamic(PxTransform(PxVec3(0, 20, 100)), PxSphereGeometry(5), PxVec3(0, -25, -100));
+	PxRigidBodyExt::updateMassAndInertia(*ball, 1000.f);
+
+	initVehicle();
+}
+
+PhysicsEngine::~PhysicsEngine()
+{
+	gFoundation->release();
+	gDispatcher->release();
+	gScene->release();
+	gMaterial->release();
+	//gCudaContextManager->release();
+	ball->release();
+
+	gVehicle4W->getRigidDynamicActor()->release();
+	gVehicle4W->free();
+	gVehicleSceneQueryData->free(gAllocator);
+
+	if (gPvd)
+	{
+		PxPvdTransport* transport = gPvd->getTransport();
+		gPvd->release();	gPvd = NULL;
+	}
+
+	printf("SnippetVehicle4W done.\n");
+}
+
+//possible time update function here. Need a time object or a way to track time/ticks
+
+PxVec3 PhysicsEngine::GetPosition()
+{
+	PxVec3 pos = gVehicle4W->getRigidDynamicActor()->getGlobalPose().p;
+	return pos;
+}
+
+PxRigidDynamic* PhysicsEngine::CreateDynamic(const physx::PxTransform& t, const physx::PxGeometry& geometry, const physx::PxVec3& velocity)
+{
+	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
+	dynamic->setAngularDamping(0.5f);
+	dynamic->setLinearVelocity(velocity);
+	gScene->addActor(*dynamic);
+	return dynamic;
+}
+
+snippetvehicle::VehicleDesc PhysicsEngine::initVehicleDesc()
+{
+	//Set up the chassis mass, dimensions, moment of inertia, and center of mass offset.
+	//The moment of inertia is just the moment of inertia of a cuboid but modified for easier steering.
+	//Center of mass offset is 0.65m above the base of the chassis and 0.25m towards the front.
+	const PxF32 chassisMass = 1500.0f;
+	const PxVec3 chassisDims(2.5f, 2.0f, 5.0f);
+	const PxVec3 chassisMOI
+	((chassisDims.y * chassisDims.y + chassisDims.z * chassisDims.z) * chassisMass / 12.0f,
+		(chassisDims.x * chassisDims.x + chassisDims.z * chassisDims.z) * 0.8f * chassisMass / 12.0f,
+		(chassisDims.x * chassisDims.x + chassisDims.y * chassisDims.y) * chassisMass / 12.0f);
+	const PxVec3 chassisCMOffset(0.0f, -chassisDims.y * 0.5f + 0.65f, 0.25f);
+
+	//Set up the wheel mass, radius, width, moment of inertia, and number of wheels.
+	//Moment of inertia is just the moment of inertia of a cylinder.
+	const PxF32 wheelMass = 20.0f;
+	const PxF32 wheelRadius = 0.5f;
+	const PxF32 wheelWidth = 0.4f;
+	const PxF32 wheelMOI = 0.5f * wheelMass * wheelRadius * wheelRadius;
+	const PxU32 nbWheels = 6;
+
+	VehicleDesc vehicleDesc;
+
+	vehicleDesc.chassisMass = chassisMass;
+	vehicleDesc.chassisDims = chassisDims;
+	vehicleDesc.chassisMOI = chassisMOI;
+	vehicleDesc.chassisCMOffset = chassisCMOffset;
+	vehicleDesc.chassisMaterial = gMaterial;
+	vehicleDesc.chassisSimFilterData = PxFilterData(COLLISION_FLAG_CHASSIS, COLLISION_FLAG_CHASSIS_AGAINST, 0, 0);
+
+	vehicleDesc.wheelMass = wheelMass;
+	vehicleDesc.wheelRadius = wheelRadius;
+	vehicleDesc.wheelWidth = wheelWidth;
+	vehicleDesc.wheelMOI = wheelMOI;
+	vehicleDesc.numWheels = nbWheels;
+	vehicleDesc.wheelMaterial = gMaterial;
+	vehicleDesc.chassisSimFilterData = PxFilterData(COLLISION_FLAG_WHEEL, COLLISION_FLAG_WHEEL_AGAINST, 0, 0);
+
+	return vehicleDesc;
+}
+
+void PhysicsEngine::initVehicle()
+{
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	gDriveModeOrder[0] = DriveMode::eDRIVE_MODE_BRAKE;
+	gDriveModeOrder[1] = DriveMode::eDRIVE_MODE_ACCEL_FORWARDS;
+	gDriveModeOrder[2] = DriveMode::eDRIVE_MODE_BRAKE;
+	gDriveModeOrder[3] = DriveMode::eDRIVE_MODE_ACCEL_REVERSE;
+	gDriveModeOrder[4] = DriveMode::eDRIVE_MODE_BRAKE;
+	gDriveModeOrder[5] = DriveMode::eDRIVE_MODE_HARD_TURN_LEFT;
+	gDriveModeOrder[6] = DriveMode::eDRIVE_MODE_BRAKE;
+	gDriveModeOrder[7] = DriveMode::eDRIVE_MODE_HARD_TURN_RIGHT;
+	gDriveModeOrder[8] = DriveMode::eDRIVE_MODE_ACCEL_FORWARDS;
+	gDriveModeOrder[9] = DriveMode::eDRIVE_MODE_HANDBRAKE_TURN_LEFT;
+	gDriveModeOrder[10] = DriveMode::eDRIVE_MODE_ACCEL_FORWARDS;
+	gDriveModeOrder[11] = DriveMode::eDRIVE_MODE_HANDBRAKE_TURN_RIGHT;
+	gDriveModeOrder[12] = DriveMode::eDRIVE_MODE_NONE;
+
+	gSteerVsForwardSpeedData[0] = 0.0f;
+	gSteerVsForwardSpeedData[1] = 0.75f;
+	gSteerVsForwardSpeedData[2] = 5.0f;
+	gSteerVsForwardSpeedData[3] = 0.75f;
+	gSteerVsForwardSpeedData[4] = 30.0f;
+	gSteerVsForwardSpeedData[5] = 0.125f;
+	gSteerVsForwardSpeedData[6] = 120.0f;
+	gSteerVsForwardSpeedData[7] = 0.1f;
+	gSteerVsForwardSpeedData[8] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[9] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[10] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[11] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[12] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[13] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[14] = PX_MAX_F32;
+	gSteerVsForwardSpeedData[15] = PX_MAX_F32;
+
+	gKeySmoothingData =
+	{
+		{
+			6.0f,	//rise rate eANALOG_INPUT_ACCEL
+			6.0f,	//rise rate eANALOG_INPUT_BRAKE		
+			6.0f,	//rise rate eANALOG_INPUT_HANDBRAKE	
+			2.5f,	//rise rate eANALOG_INPUT_STEER_LEFT
+			2.5f,	//rise rate eANALOG_INPUT_STEER_RIGHT
+		},
+		{
+			10.0f,	//fall rate eANALOG_INPUT_ACCEL
+			10.0f,	//fall rate eANALOG_INPUT_BRAKE		
+			10.0f,	//fall rate eANALOG_INPUT_HANDBRAKE	
+			5.0f,	//fall rate eANALOG_INPUT_STEER_LEFT
+			5.0f	//fall rate eANALOG_INPUT_STEER_RIGHT
+		}
+	};
+
+	gPadSmoothingData =
+	{
+		{
+			6.0f,	//rise rate eANALOG_INPUT_ACCEL
+			6.0f,	//rise rate eANALOG_INPUT_BRAKE		
+			6.0f,	//rise rate eANALOG_INPUT_HANDBRAKE	
+			2.5f,	//rise rate eANALOG_INPUT_STEER_LEFT
+			2.5f,	//rise rate eANALOG_INPUT_STEER_RIGHT
+		},
+		{
+			10.0f,	//fall rate eANALOG_INPUT_ACCEL
+			10.0f,	//fall rate eANALOG_INPUT_BRAKE		
+			10.0f,	//fall rate eANALOG_INPUT_HANDBRAKE	
+			5.0f,	//fall rate eANALOG_INPUT_STEER_LEFT
+			5.0f	//fall rate eANALOG_INPUT_STEER_RIGHT
+		}
+	};
+
+	PxFixedSizeLookupTable<8> gSteerVsForwardSpeedTable(gSteerVsForwardSpeedData, 4);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+
+	PxU32 numWorkers = 1;
+	//gDispatcher = PxDefaultCpuDispatcherCreate(numWorkers);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = VehicleFilterShader;
+
+	//gScene = gPhysics->createScene(sceneDesc);
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	gCooking = PxCreateCooking(PX_PHYSICS_VERSION, *gFoundation, PxCookingParams(PxTolerancesScale()));
+
+	/////////////////////////////////////////////
+
+	PxInitVehicleSDK(*gPhysics);
+	PxVehicleSetBasisVectors(PxVec3(0, 1, 0), PxVec3(0, 0, 1));
+	PxVehicleSetUpdateMode(PxVehicleUpdateMode::eVELOCITY_CHANGE);
+
+	//Create the batched scene queries for the suspension raycasts.
+	gVehicleSceneQueryData = VehicleSceneQueryData::allocate(1, PX_MAX_NB_WHEELS, 1, 1, WheelSceneQueryPreFilterBlocking, NULL, gAllocator);
+	gBatchQuery = VehicleSceneQueryData::setUpBatchedSceneQuery(0, *gVehicleSceneQueryData, gScene);
+
+	//Create the friction table for each combination of tire and surface type.
+	gFrictionPairs = createFrictionPairs(gMaterial);
+
+	//Create a plane to drive on.
+	PxFilterData groundPlaneSimFilterData(COLLISION_FLAG_GROUND, COLLISION_FLAG_GROUND_AGAINST, 0, 0);
+	gGroundPlane = createDrivablePlane(groundPlaneSimFilterData, gMaterial, gPhysics);
+	gScene->addActor(*gGroundPlane);
+
+	//Create a vehicle that will drive on the plane.
+	VehicleDesc vehicleDesc = initVehicleDesc();
+	gVehicle4W = createVehicle4W(vehicleDesc, gPhysics, gCooking);
+	PxTransform startTransform(PxVec3(0, (vehicleDesc.chassisDims.y * 0.5f + vehicleDesc.wheelRadius + 1.0f), 0), PxQuat(PxIdentity));
+	gVehicle4W->getRigidDynamicActor()->setGlobalPose(startTransform);
+	gScene->addActor(*gVehicle4W->getRigidDynamicActor());
+
+	//Set the vehicle to rest in first gear.
+	//Set the vehicle to use auto-gears.
+	gVehicle4W->setToRestState();
+	gVehicle4W->mDriveDynData.forceGearChange(PxVehicleGearsData::eFIRST);
+	gVehicle4W->mDriveDynData.setUseAutoGears(true);
+
+	gVehicleModeTimer = 0.0f;
+	gVehicleOrderProgress = 0;
+	startBrakeMode();
+}
 
 
-//main physics class. Acts as the buffer between the game and PhysX
-/*
-	Should hold a list of all physics objects and updates them accordingly
+void PhysicsEngine::incrementDrivingMode(const PxF32 timestep)
+{
+	gVehicleModeTimer += timestep;
+	if (gVehicleModeTimer > gVehicleModeLifetime)
+	{
+		//If the mode just completed was eDRIVE_MODE_ACCEL_REVERSE then switch back to forward gears.
+		if (DriveMode::eDRIVE_MODE_ACCEL_REVERSE == gDriveModeOrder[gVehicleOrderProgress])
+		{
+			gVehicle4W->mDriveDynData.forceGearChange(PxVehicleGearsData::eFIRST);
+		}
 
-	Individual objects with physics should have the physx type applied to them and managed through here
+		//Increment to next driving mode.
+		gVehicleModeTimer = 0.0f;
+		gVehicleOrderProgress++;
+		releaseAllControls();
 
-	Think of this as the main() for physics
+		//If we are at the end of the list of driving modes then start again.
+		if (DriveMode::eDRIVE_MODE_NONE == gDriveModeOrder[gVehicleOrderProgress])
+		{
+			gVehicleOrderProgress = 0;
+			gVehicleOrderComplete = true;
+		}
 
-	Should also hold the PVD code for visualization
-*/
+		//Start driving in the selected mode.
+		DriveMode eDriveMode = gDriveModeOrder[gVehicleOrderProgress];
+		switch (eDriveMode)
+		{
+		case DriveMode::eDRIVE_MODE_ACCEL_FORWARDS:
+			startAccelerateForwardsMode();
+			break;
+		case DriveMode::eDRIVE_MODE_ACCEL_REVERSE:
+			startAccelerateReverseMode();
+			break;
+		case DriveMode::eDRIVE_MODE_HARD_TURN_LEFT:
+			startTurnHardLeftMode();
+			break;
+		case DriveMode::eDRIVE_MODE_HANDBRAKE_TURN_LEFT:
+			startHandbrakeTurnLeftMode();
+			break;
+		case DriveMode::eDRIVE_MODE_HARD_TURN_RIGHT:
+			startTurnHardRightMode();
+			break;
+		case DriveMode::eDRIVE_MODE_HANDBRAKE_TURN_RIGHT:
+			startHandbrakeTurnRightMode();
+			break;
+		case DriveMode::eDRIVE_MODE_BRAKE:
+			startBrakeMode();
+			break;
+		case DriveMode::eDRIVE_MODE_NONE:
+			break;
+		};
+
+		//If the mode about to start is eDRIVE_MODE_ACCEL_REVERSE then switch to reverse gears.
+		if (DriveMode::eDRIVE_MODE_ACCEL_REVERSE == gDriveModeOrder[gVehicleOrderProgress])
+		{
+			gVehicle4W->mDriveDynData.forceGearChange(PxVehicleGearsData::eREVERSE);
+		}
+	}
+}
+
+void PhysicsEngine::startAccelerateForwardsMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalAccel(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogAccel(1.0f);
+	}
+}
+
+void PhysicsEngine::startAccelerateReverseMode()
+{
+	gVehicle4W->mDriveDynData.forceGearChange(PxVehicleGearsData::eREVERSE);
+
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalAccel(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogAccel(1.0f);
+	}
+}
+
+void PhysicsEngine::startBrakeMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalBrake(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogBrake(1.0f);
+	}
+}
+
+void PhysicsEngine::startTurnHardLeftMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalAccel(true);
+		gVehicleInputData.setDigitalSteerLeft(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogAccel(true);
+		gVehicleInputData.setAnalogSteer(-1.0f);
+	}
+}
+
+void PhysicsEngine::startTurnHardRightMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalAccel(true);
+		gVehicleInputData.setDigitalSteerRight(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogAccel(1.0f);
+		gVehicleInputData.setAnalogSteer(1.0f);
+	}
+}
+
+void PhysicsEngine::startHandbrakeTurnLeftMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalSteerLeft(true);
+		gVehicleInputData.setDigitalHandbrake(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogSteer(-1.0f);
+		gVehicleInputData.setAnalogHandbrake(1.0f);
+	}
+}
+
+void PhysicsEngine::startHandbrakeTurnRightMode()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalSteerRight(true);
+		gVehicleInputData.setDigitalHandbrake(true);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogSteer(1.0f);
+		gVehicleInputData.setAnalogHandbrake(1.0f);
+	}
+}
+
+
+void PhysicsEngine::releaseAllControls()
+{
+	if (gMimicKeyInputs)
+	{
+		gVehicleInputData.setDigitalAccel(false);
+		gVehicleInputData.setDigitalSteerLeft(false);
+		gVehicleInputData.setDigitalSteerRight(false);
+		gVehicleInputData.setDigitalBrake(false);
+		gVehicleInputData.setDigitalHandbrake(false);
+	}
+	else
+	{
+		gVehicleInputData.setAnalogAccel(0.0f);
+		gVehicleInputData.setAnalogSteer(0.0f);
+		gVehicleInputData.setAnalogBrake(0.0f);
+		gVehicleInputData.setAnalogHandbrake(0.0f);
+	}
+}
+
+void PhysicsEngine::stepPhysics()
+{
+	const PxF32 timestep = 1.0f / 60.0f;
+
+	//Cycle through the driving modes to demonstrate how to accelerate/reverse/brake/turn etc.
+	incrementDrivingMode(timestep);
+
+	//Update the control inputs for the vehicle.
+	if (gMimicKeyInputs)
+	{
+		PxVehicleDrive4WSmoothDigitalRawInputsAndSetAnalogInputs(gKeySmoothingData, gSteerVsForwardSpeedTable, gVehicleInputData, timestep, gIsVehicleInAir, *gVehicle4W);
+	}
+	else
+	{
+		PxVehicleDrive4WSmoothAnalogRawInputsAndSetAnalogInputs(gPadSmoothingData, gSteerVsForwardSpeedTable, gVehicleInputData, timestep, gIsVehicleInAir, *gVehicle4W);
+	}
+
+	//Raycasts.
+	PxVehicleWheels* vehicles[1] = { gVehicle4W };
+	PxRaycastQueryResult* raycastResults = gVehicleSceneQueryData->getRaycastQueryResultBuffer(0);
+	const PxU32 raycastResultsSize = gVehicleSceneQueryData->getQueryResultBufferSize();
+	PxVehicleSuspensionRaycasts(gBatchQuery, 1, vehicles, raycastResultsSize, raycastResults);
+
+	//Vehicle update.
+	const PxVec3 grav = gScene->getGravity();
+	PxWheelQueryResult wheelQueryResults[PX_MAX_NB_WHEELS];
+	PxVehicleWheelQueryResult vehicleQueryResults[1] = { {wheelQueryResults, gVehicle4W->mWheelsSimData.getNbWheels()} };
+	PxVehicleUpdates(timestep, grav, *gFrictionPairs, 1, vehicles, vehicleQueryResults);
+
+	//Work out if the vehicle is in the air.
+	gIsVehicleInAir = gVehicle4W->getRigidDynamicActor()->isSleeping() ? false : PxVehicleIsInAir(vehicleQueryResults[0]);
+
+	//Scene update.
+	gScene->simulate(timestep);
+	gScene->fetchResults(true);
+}
+
+void PhysicsEngine::keyPress(unsigned char key, const PxTransform& camera)
+{
+	PX_UNUSED(camera);
+	PX_UNUSED(key);
+}
+
