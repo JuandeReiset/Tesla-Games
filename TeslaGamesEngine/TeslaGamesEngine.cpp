a4fbff5c1761e5413f48185f200fabde7e5bc260
JuandeReiset <juanluisreiset@gmail.com>
2020-02-11 01:47:01 -0700

    Added basic track to the game loop execution with respective textures

--- TeslaGamesEngine/TeslaGamesEngine.cpp
+++ TeslaGamesEngine/TeslaGamesEngine.cpp
@@ -491,8 +491,8 @@ int main()
 
 		// Draw racing track
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(-7.0f, 0.0f, 10.0f));
-		//model = glm::scale(model, glm::vec3(0.006f, 0.006f, 0.006f));
+		model = glm::translate(model, glm::vec3(0.0f, -10.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(7.f,7.f, 7.f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		racetrack.RenderModel();
