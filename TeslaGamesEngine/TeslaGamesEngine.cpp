5efa08c035fefc436bf9f130587aa80b56397547
YZ-YI <yiyuzhe0125@outlook.com>
2020-02-14 00:13:39 -0700

    changed track size

--- TeslaGamesEngine/TeslaGamesEngine.cpp
+++ TeslaGamesEngine/TeslaGamesEngine.cpp
@@ -826,7 +826,7 @@ int main()
 		// Draw racing track
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(7.f,7.f, 7.f));
+		model = glm::scale(model, glm::vec3(10.f,10.f, 10.f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		racetrack.RenderModel();
