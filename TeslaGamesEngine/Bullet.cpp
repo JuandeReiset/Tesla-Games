b7905974758c2b5754f0a2b3b388eb1a9aeb17b8
JuandeReiset <juanluisreiset@gmail.com>
2020-03-04 13:36:56 -0700

    Tweaked bullet.cpp (shooting bullets class)

--- TeslaGamesEngine/Bullet.cpp
+++ TeslaGamesEngine/Bullet.cpp
@@ -47,7 +47,7 @@ void Bullet::renderBullet() {
 	shoot_distance_z += Direction_z * bullet_speed;
 
 	model = glm::mat4(1.0f);
-	model = glm::translate(model, glm::vec3(position.x + shoot_distance_x, position.y + 0.5f, position.z + shoot_distance_z));
+	model = glm::translate(model, glm::vec3(position.x + shoot_distance_x, position.y + 0.6f, position.z + shoot_distance_z));
 
 	model = glm::scale(model, glm::vec3(0.3f, 0.3f, 0.3f));
 	
