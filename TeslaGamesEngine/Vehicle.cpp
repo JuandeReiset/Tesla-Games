dbe70e93c012f978ab21efc3cc5f1e6bbc9a0f12
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-03-08 13:15:16 -0600

    Fixed vehicle rotation at start

--- TeslaGamesEngine/Vehicle.cpp
+++ TeslaGamesEngine/Vehicle.cpp
@@ -315,7 +315,7 @@ void Vehicle::initVehicle(PxPhysics* gPhysics, PxCooking* gCooking, PxMaterial* 
 	//Create a vehicle that will drive on the plane.
 	VehicleDesc vehicleDesc = initVehicleDesc(gMaterial);
 	gVehicle4W = createVehicle4W(vehicleDesc, gPhysics, gCooking);
-	PxTransform startTransform(PxVec3(0, (vehicleDesc.chassisDims.y * 0.5f + vehicleDesc.wheelRadius + 1.0f), 0), PxQuat(PxIdentity));
+	PxTransform startTransform(PxVec3(0, (vehicleDesc.chassisDims.y * 0.5f + vehicleDesc.wheelRadius + 1.0f), 0), PxQuat(-1.5708f, PxVec3(0, 1, 0)));
 	startTransform.p = position;	//dear lord I hope this works
 	actor = gVehicle4W->getRigidDynamicActor();	//set vehicle dynamic to the PhysxObject actor
 	actor->setName("vehicle");
