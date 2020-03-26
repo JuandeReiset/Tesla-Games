7b30ba518639ae577978e64be1198b37667bfc8f
JuandeReiset <juanluisreiset@gmail.com>
2020-03-26 15:26:52 -0600

    Set up for raycast_shooting

--- TeslaGamesEngine/Raycast_shooting.h
+++ TeslaGamesEngine/Raycast_shooting.h
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "Vehicle.h"
+#include <vector>
 #include <glm\glm.hpp>
 
 class Raycast_shooting
 {
+public:
+
+
+private:
+	glm::vec3 Start_pos;
+	glm::vec3  Ray_direction;
+
+	std::vector<Vehicle*> vehicles;
+	Vehicle* owner;
+	Vehicle* target;
+   
 };
 
