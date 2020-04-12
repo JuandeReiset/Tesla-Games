dd8a312740954d906b62b14edc59ae2fce6bbe4a
Asjad <Asjad@PHANTOM>
2020-04-11 22:53:17 -0600

    Added new build commands

--- TeslaGamesBuild.bat
+++
@@ -1,6 +0,0 @@
-xcopy "$(SolutionDir)\dlls\*" "$(OutDir)" /K /D /H /Y
-xcopy "$(SolutionDir)\TeslaGamesEngine\audioFiles\*" "$(OutDir)audioFiles\" /K /D /H /Y /S /I
-xcopy "$(SolutionDir)\TeslaGamesEngine\ImGui\*" "$(OutDir)ImGui\" /K /D /H /Y /S /I
-xcopy "$(SolutionDir)\TeslaGamesEngine\Models\*" "$(OutDir)Models\" /K /D /H /Y /S /I
-xcopy "$(SolutionDir)\TeslaGamesEngine\Shaders\*" "$(OutDir)Shaders\" /K /D /H /Y /S /I
-xcopy "$(SolutionDir)\TeslaGamesEngine\Textures\*" "$(OutDir)Textures\" /K /D /H /Y /S /I
